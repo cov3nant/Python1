968fff04371aafa56bc808642cd65c7ca287aa73
cov3nant <agetorgodsway@gmail.com>
2022-08-29 12:14:36 +0000

    first commit

---
+++ average_calculator.py
@@ -0,0 +1,13 @@
+all_inputs= []
+while True:
+    user_inputs=input('Please type in a number:  ')
+    if user_inputs == 'done':
+        break
+    try:
+        converted_inputs=int(user_inputs)
+        all_inputs.append(converted_inputs)
+    except:
+        print('Invalid input')
+total=sum(all_inputs)
+average=total/(len(all_inputs))
+print('Total: ', total,' | Number of Inputs: ', len(all_inputs), ' | Average: ', average)
